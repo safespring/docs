a6161c2216bd4d2f3c552c1fe3c3f6051ae6d609
Jan Ivar Beddari <code@beddari.net>
2017-04-21 14:35:51 +0200

    Fix PWD var

--- dev.sh
+++ dev.sh
@@ -1,1 +1,2 @@
-docker run --rm -it -p 8000:8000 -v /home/beddari/work/docs:/docs:z squidfunk/mkdocs-material
+#/bin/bash -x
+docker run --rm -it -p 8000:8000 -v ${PWD}:/docs:z squidfunk/mkdocs-material
